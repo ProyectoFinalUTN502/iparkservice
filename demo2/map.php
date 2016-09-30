0eb4b66ede9f529f06408aed0282f7f933f26056
ProyectoFinalUTN502 <cesarcappetto@gmail.com>
2016-09-29 23:58:33 -0300

    Se finaliza la Demo 2

--- demo2/map.php
+++ demo2/map.php
@@ -7,8 +7,8 @@
             }
 
             table {
-                width: 50%;
-                height:50%;
+                width: 100%;
+                height:100%;
                 border-collapse: collapse;
             }
 
