62815ec9cf1b3d61ef17a2af1058ddf75b39de1b
ProyectoFinalUTN502 <cesarcappetto@gmail.com>
2016-10-31 11:36:48 -0300

    Sensor Inductivo finalizado

--- sensor.php
+++ sensor.php
@@ -6,7 +6,7 @@ require_once "selectors/layoutSelector.php";
 $layoutID = filter_input(INPUT_GET, "id");
 
 if ($layoutID == NULL) {
-    echo "<h1>No se encontro un valor de Layout</h1>";
+    echo "<h2>No se encontro un valor de Layout</h2>";
     echo "<p>Debe ingresar el identificador de Layout para poder cargar"
     . "el mapa de este</p>";
     exit();
@@ -16,15 +16,20 @@ $sql    = "SELECT * FROM layout WHERE id = " . $layoutID . " LIMIT 1";
 $op     = executeQuery($sql);
 $row    = $op->fetch_assoc();
 
-$maxRows    = $row["maxRows"];
-$maxCols    = $row["maxCols"];
+if ($row == NULL) {
+    echo "<h2>No existe un Layout con ese Identificador</h2>";
+    echo "<p>Debe ingresar el identificador de Layout para poder cargar"
+    . "el mapa de este</p>";
+    exit();
+}
 
+$maxRows = $row["maxRows"];
+$maxCols = $row["maxCols"];
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <!--<meta charset='UTF-8' content='1' http-equiv='REFRESH'></meta>-->
-        <title></title>
+        <title>Sensor Inductivo</title>
         <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
         <style>
             .layoutTable { width: 50%; border-collapse: collapse; table-layout: fixed; }
@@ -39,35 +44,68 @@ $maxCols    = $row["maxCols"];
                 for ($j = 0; $j < $maxCols; $j++) {
                     $style = "";
                     $text = "";
+                    $state = "data-state=''";
                     
                     $pos = getControlPosition($layoutID, $i, $j);
                     
                     // Posiciones Validas para Estacionar
                     if ($pos["valid"] == VALID_POSITION && $pos["vehicle_type_id"] != "") {
-                        
-                        if ($pos["state"] == LAYOUT_FREE) {
-                            $style = "style='background-color:#8cff66;'";
-                            $text = "<b>" . $pos["name"] ."</b>";
-                        } elseif ($pos["state"] == LAYOUT_BOOKED) {
-                            $style = "style='background-color:#ff4d4d;'";
-                            $text = "<b>" . $pos["name"] ."</b>";
+                        switch($pos["state"]) {
+                            case LAYOUT_FREE:
+                                $state  = "data-state='". LAYOUT_FREE ."'";
+                                $style  = "style='background-color:#8cff66;'";
+                                $text   = "<b>" . $pos["name"] ."</b>";
+                                break;
+                            case LAYOUT_BOOKED:
+                                $state  = "data-state='" . LAYOUT_BOOKED . "'";
+                                $style  = "style='background-color:#ff4d4d;'";
+                                $text   = "<b>" . $pos["name"] ."</b>";
+                                break;
+                            case LAYOUT_UNAVAILABLE:
+                                $state  = "data-state='" . LAYOUT_UNAVAILABLE . "'";
+                                $style  = "style='background-color:#000000; color:#ffffff;'";
+                                $text   = "<b>" . $pos["name"] ."</b>";
+                                break;
+                            default:
+                                break;
                         }
-                        
+                    }
+                    
+                    // Posicion de Entrada 
+                    if ($pos["din"] == ACTIVE && $pos["valid"] == INVALID_POSITION && $pos["vehicle_type_id"] == "") {
+                        // #66bfff
+                        $style  = "style='background-color:#66bfff;'";
+                        $text   = "<b>ENTRADA</b>";
+                    }
+                    
+                    // Posicion de Salida
+                    if ($pos["dout"] == ACTIVE && $pos["valid"] == INVALID_POSITION && $pos["vehicle_type_id"] == "") {
+                        $style  = "style='background-color:#fff466;'";
+                        $text   = "<b>SALIDA</b>";
+                    }
+                    
+                    // Rampa de Subida
+                    if ($pos["rIn"] == ACTIVE && $pos["valid"] == INVALID_POSITION && $pos["vehicle_type_id"] == "") {
+                        $style  = "style='background-color:#d366ff;'";
+                        $text   = "<b>&#62;</b>";
+                    }
+                    
+                    // Rampa de Bajada
+                    if ($pos["rOut"] == ACTIVE && $pos["valid"] == INVALID_POSITION && $pos["vehicle_type_id"] == "") {
+                        $style  = "style='background-color:#d366ff;'";
+                        $text   = "<b>&#60;</b>";
                     }
                     
                     // Posiciones No Validas para Estacionar
-                    if ($pos["valid"] == INVALID_POSITION && $pos["vehicle_type_id"] == "") {
+                    $exp = $pos["dout"] == NOT_ACTIVE && $pos["din"] == NOT_ACTIVE && 
+                            $pos["rOut"] == NOT_ACTIVE && $pos["rIn"] == NOT_ACTIVE && 
+                            $pos["valid"] == INVALID_POSITION && $pos["vehicle_type_id"] == "";
+                    if ($exp) {
                         $style = "style='background-color:#909090;'";
                         $text = "";
                     }
                     
-                    // Posiciones Ocupadas por otro Auto
-                    if ($pos["state"] == LAYOUT_UNAVAILABLE) {
-                        $style = "style='background-color:#000000;'";
-                        $text = "";
-                    }
-                    
-                    echo "<td data-id='" . $pos["id"] . "' " . $style ." align='center'>" . $text . "</td>";
+                    echo "<td " . $state . " data-id='" . $pos["id"] . "' " . $style ." align='center'>" . $text . "</td>";
                 }
                 echo "</tr>";
             }
@@ -78,48 +116,52 @@ $maxCols    = $row["maxCols"];
 </html>
 <script>
     $("#layoutTable").on("click", "td", function() {
-        var id = $( this ).data("id");
-        var color = hexc($( this ).css("background-color"));
+        var id          = $( this ).data("id");
+        var state       = $( this ).data("state");
+        var nextState   = "";
         
-        // Posicion Libre
-        if (color === "#8cff66") {
-            $(this).css("background-color", "#ff4d4d");  
-            toogleLp(id, <?php echo "'" . LAYOUT_BOOKED . "'"; ?>);
+        switch (state) {
+            case "<?php echo LAYOUT_FREE; ?>":
+                        
+                nextState = "<?php echo LAYOUT_BOOKED; ?>";
+                $( this ).css("background-color","#ff4d4d");
+                $( this ).data("state", nextState);
+                toogleLp(id, nextState);
+                console.log(nextState);
+                break;
+                
+            case "<?php echo LAYOUT_BOOKED; ?>":
+                        
+                nextState = "<?php echo LAYOUT_UNAVAILABLE; ?>";
+                $( this ).css("background-color","#000000");
+                $( this ).css("color","#ffffff");
+                $( this ).data("state", nextState);
+                toogleLp(id, nextState);
+                console.log(nextState);
+                break;
+                
+            case "<?php echo LAYOUT_UNAVAILABLE; ?>":
+                        
+                nextState = "<?php echo LAYOUT_FREE; ?>";
+                $( this ).css("background-color","#8cff66");
+                $( this ).css("color","#000000");
+                $( this ).data("state", nextState);
+                toogleLp(id, nextState);
+                console.log(nextState);
+                break;
+                
+            default:
+                break;
         }
-        
-        // Posicion Reservada
-        if (color === "#ff4d4d") {
-            $(this).css("background-color", "#8cff66"); 
-            toogleLp(id, <?php echo "'" . LAYOUT_FREE . "'"; ?>);
-        }
-        
-        // Posicion Ocupada
-        if (color === "#000000") {
-            $(this).css("background-color", "#8cff66"); 
-            toogleLp(id, <?php echo "'" . LAYOUT_FREE . "'"; ?>);
-        }
-         
    });   
-   
-    function hexc(colorval) {
-        var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-        delete(parts[0]);
-        for (var i = 1; i <= 3; ++i) {
-            parts[i] = parseInt(parts[i]).toString(16);
-            if (parts[i].length == 1) parts[i] = '0' + parts[i];
-        }
-        color = '#' + parts.join('');
 
-        return color;
-    }
-    
     function toogleLp(id, state)
     {
         var parametros = {"id": id, "state" : state};
 
         $.ajax({
             data: parametros,
-            url: "control_ajax.php",
+            url: "sensorTogle.php",
             type: 'POST',
             beforeSend: function () { },
             success: function () {
